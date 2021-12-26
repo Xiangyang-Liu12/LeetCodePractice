9f22d9c0a632d7450fb6ee5cf28cd70a2aca3bd6
xiangyang <xiangyang9812@gmail.com>
2021-12-25 19:58:43 -0800

    first

--- Tree/298. Binary Tree Longest Consecutive Sequence.java
+++ Tree/298. Binary Tree Longest Consecutive Sequence.java
@@ -1,34 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+package Tree;
+
 class Solution {
-    private int max = 0;
+    int max = 0;
     public int longestConsecutive(TreeNode root) {
-       if(root == null) return 0;
-       helper(root, 0, root.val);
-       return max;
+        if(root == null) return 0;
+        helper(root, 0, root.val);
+        return max;
     }
-    pulic void helper(TreeNode root, int current, int target){
+    public void helper(TreeNode root, int current, int target){
         if(root == null) return;
         if(root.val == target){
             current++;
         }else{
             current = 1;
         }
-        max = Math.max(current, result);
-        helper(root.left, current, root.val+1);
-        helper(root.right, current, root.val+1);
+        max = Math.max(max, current);
+        helper(root.left, current, root.val + 1);
+        helper(root.right, current, root.val + 1);
     }
 }
